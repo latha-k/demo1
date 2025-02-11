7fd6ea934dbbaa94bce7df4fade973ae18632479
latha-k <lathajava1@gmail.com>
2025-02-10 17:55:22 -0700

    commit from i d l1

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!1");
-        System.out.printf("Hello and welcome!1");
+
         System.out.printf("Hello and welcome!2");
         System.out.printf("Hello and welcome!3");
         System.out.printf("Hello and welcome!4");
